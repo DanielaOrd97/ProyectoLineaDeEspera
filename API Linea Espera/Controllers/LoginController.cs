db63e6865a66590736781203a04e9bbc2ccf1df0
Abraham Torres <abraham.torres@r2devpros.com>
2024-06-05 00:34:48 -0600

    AGREGADO LOGIN CONTROLLER Y HELPER TOKEN JWT

---
+++ API Linea Espera/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using API_Linea_Espera.Helpers;
+using API_Linea_Espera.Models.DTOs;
+using API_Linea_Espera.Models.Entities;
+using API_Linea_Espera.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Linea_Espera.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : Controller
+    {
+        public IRepository<Usuarios> RepositoryUsuarios { get; }
+        public IRepository<Cajas> RepositoryCajas { get; }
+        public IRepository<Roles> RepositoryRoles { get; }
+        public LoginController(IRepository<Usuarios> repositoryUsuarios, 
+            IRepository<Cajas> repositoryCajas, IRepository<Roles> repositoryRoles)
+        {
+            this.RepositoryUsuarios = repositoryUsuarios;
+            this.RepositoryCajas = repositoryCajas;
+            this.RepositoryRoles = repositoryRoles;
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(UsuarioDTO usuarioDTO)
+        {
+            if (string.IsNullOrWhiteSpace(usuarioDTO.NombreUsuario))
+            {
+                ModelState.AddModelError("", "Proporcione el nombre de usuario para iniciar sesión");
+                return BadRequest(ModelState);
+
+            }
+            if (string.IsNullOrWhiteSpace(usuarioDTO.Contraseña))
+            {
+                ModelState.AddModelError("", "Proporcione la contraseña para iniciar sesión");
+                return BadRequest(ModelState);
+            }
+            var usuario = RepositoryUsuarios.GetAllWithInclude()
+                .First(x => (x.NombreUsuario == usuarioDTO.NombreUsuario) && (x.Contraseña ==usuarioDTO.Contraseña));
+
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var rol = RepositoryRoles.GetAllWithInclude().Where(x => x.IdRol == usuario.IdRol);
+
+                TokenGeneratorJwt jwtToken = new();
+                // TODO: CAMBIAR ROL
+                return Ok(jwtToken.GetToken(usuario.Nombre, "Administrador", usuario.Id.ToString()));
+                
+            }
+                
+
+        }
+    }
+}
---
+++ API Linea Espera/Helpers/TokenGeneratorJwt.cs
@@ -0,0 +1,52 @@
+﻿using API_Linea_Espera.Models.DTOs;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace API_Linea_Espera.Helpers
+{
+    public class TokenGeneratorJwt
+    {
+
+        public string GetToken(string nombre, string rol, string id)
+        {
+            List<Claim> claims = new();
+
+            if (nombre=="Administrador")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "Administrador"));
+            }
+            else if(rol=="Operador")
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "Operador"));
+            }
+            else
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "Cliente"));
+            }
+            claims.Add(new Claim(ClaimTypes.Name, nombre));
+            claims.Add(new Claim(ClaimTypes.Role, rol));
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, id));
+
+
+            JwtSecurityTokenHandler handler = new();
+
+            var token = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Issuer = "lienaespera.net",
+                Audience = "AppLineaEspera",
+                IssuedAt = DateTime.Now,
+                Expires = DateTime.Now.AddDays(1),
+                NotBefore = DateTime.Now.AddMinutes(-1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes("LALLAVESECRETADELBANCOESESTA")),
+            SecurityAlgorithms.HmacSha256)
+            };
+
+            return handler.CreateEncodedJwt(token);
+        }
+    }
+}
