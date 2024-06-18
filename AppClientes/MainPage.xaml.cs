d3a0d33345dedd25a17957d52a204cf0b5e3f2de
AlexanderContreras17 <hatakealexander24@gmail.com>
2024-06-17 21:42:53 -0600

    Vista Maui con estilos

--- AppClientes/MainPage.xaml.cs
+++ AppClientes/MainPage.xaml.cs
@@ -9,14 +9,14 @@ namespace AppClientes
 {
     public partial class MainPage : ContentPage
     {
-        private Dictionary<string,( int counter, string estado)> serviceCounters = new Dictionary<string, (int, string)>();
+        private Dictionary<string, (int counter, string estado)> serviceCounters = new Dictionary<string, (int, string)>();
         //{
         //    {"Caja 1", 1 },
         //    {"Caja 2",1 },
         //    { "Caja 3",1 }
         //};
         private string selectedService;
-        private HttpClient httpClient= new HttpClient();
+        private HttpClient httpClient = new HttpClient();
 
         public MainPage()
         {
@@ -24,18 +24,18 @@ namespace AppClientes
             //ServicePicker.SelectedIndex = 0; //Este selecciona el primer servicio por defecto
             //selectedService = ServicePicker.Items[0];
             //UpdateNumberLabel();
-            ServicePicker.SelectedIndexChanged += OnServiceSelected;
+            //ServicePicker.SelectedIndexChanged += OnServiceSelected;
         }
 
         private void OnServiceSelected(object? sender, EventArgs e)
         {
-            if (ServicePicker.SelectedItem != null)
-            {
-                selectedService = ServicePicker.SelectedItem.ToString();
-                UpdateNumberLabel();
-            }
+            //if (ServicePicker.SelectedItem != null)
+            //{
+            //    selectedService = ServicePicker.SelectedItem.ToString();
+            //    UpdateNumberLabel();
+            ////}
         }
-      
+
 
         private async Task LoadServicesFromApi()
         {
@@ -50,15 +50,16 @@ namespace AppClientes
                 {
                     serviceCounters[service] = (1, "En Espera");//inicializar el contador para cada servicio
                 }
-                ServicePicker.ItemsSource = services;
+                //ServicePicker.ItemsSource = services;
                 if (services.Count > 0)
                 {
-                    ServicePicker.SelectedIndex = 0;
+                    //ServicePicker.SelectedIndex = 0;
                     selectedService = services[0];
                     UpdateNumberLabel();
                 }
             }
-            catch (Exception ex) {
+            catch (Exception ex)
+            {
 
                 await DisplayAlert("Error", "No se pudieron cargar los servicios: " + ex.Message, "OK");
             }
@@ -67,44 +68,46 @@ namespace AppClientes
         private void UpdateNumberLabel()
         {
             if (string.IsNullOrEmpty(selectedService)) return;
-           var (counter, estado)=serviceCounters[selectedService];
-            NumberLabel.Text = counter.ToString();
-            TurnStateLabel.Text = estado;
-        }
-
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            //verifica si hay un servicio seleccionado
-            if(string.IsNullOrEmpty(selectedService)) return;
-            var (counter,estado)=serviceCounters[selectedService];
-            counter++;
-            estado = "En Espera";
-
-            //Inrementa el contador del servicio
-            serviceCounters[selectedService] =(counter,estado);
-            //actualiza label
-            UpdateNumberLabel() ;
-        }
-
-        private void ServicePicker_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            selectedService = ServicePicker.SelectedItem.ToString();
-            UpdateNumberLabel();
-        }
-
-        private async void Button_Clicked_1(object sender, EventArgs e)
-        {
-            await LoadServicesFromApi();
-        }
-        //Este metodo es opcional si lo quieren dejar, es para cambiar el estado del turno
-        private void ChangeTurnState(string newState)
-        {
-            if (string.IsNullOrEmpty(selectedService)) return;
-            var(counter, estado)=serviceCounters[selectedService];
-            estado=newState;
-            serviceCounters[selectedService]=(counter,estado);
-            UpdateNumberLabel();
+            var (counter, estado) = serviceCounters[selectedService];
+            //    NumberLabel.Text = counter.ToString();
+            //    TurnStateLabel.Text = estado;
+            //}
         }
     }
-
 }
+//        private void Button_Clicked(object sender, EventArgs e)
+//        {
+//            //verifica si hay un servicio seleccionado
+//            if(string.IsNullOrEmpty(selectedService)) return;
+//            var (counter,estado)=serviceCounters[selectedService];
+//            counter++;
+//            estado = "En Espera";
+
+//            //Inrementa el contador del servicio
+//            serviceCounters[selectedService] =(counter,estado);
+//            //actualiza label
+//            UpdateNumberLabel() ;
+//        }
+
+//        private void ServicePicker_SelectedIndexChanged(object sender, EventArgs e)
+//        {
+//            selectedService = ServicePicker.SelectedItem.ToString();
+//            UpdateNumberLabel();
+//        }
+
+//        private async void Button_Clicked_1(object sender, EventArgs e)
+//        {
+//            await LoadServicesFromApi();
+//        }
+//        //Este metodo es opcional si lo quieren dejar, es para cambiar el estado del turno
+//        private void ChangeTurnState(string newState)
+//        {
+//            if (string.IsNullOrEmpty(selectedService)) return;
+//            var(counter, estado)=serviceCounters[selectedService];
+//            estado=newState;
+//            serviceCounters[selectedService]=(counter,estado);
+//            UpdateNumberLabel();
+//        }
+//    }
+
+//}
